d1b81372f0c1ac3fba5960198222401e18f4266e
NiravDhanani <nirav.devotee@gmail.com>
2024-07-26 17:13:25 +0530

    add select player option

--- tic toc toe/index.js
+++ tic toc toe/index.js
@@ -1,27 +1,41 @@
 let box = document.querySelectorAll("#box");
 let valX = document.querySelector("#valX");
+let player = document.querySelector("#player");
+let selectPlayer = document.querySelector("#selectPlayer");
+let selectPlayer2 = document.querySelector("#selectPlayer2");
 
 let turn = true;
 let cnt = 0;
 let winningPatterns = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
 ];
 
 valX.addEventListener("click", () => {
     if (valX.checked) {
         turn = false;
+        valX.style.display = "none"
+        player.style.display = "none"
+        selectPlayer.innerHTML = "X"
+        selectPlayer2.innerHTML = "O"
     }
 });
 
+function defaultplayer() {
+    selectPlayer.innerHTML = "O"
+    selectPlayer2.innerHTML = "X"
+}
+defaultplayer()
+
 box.forEach((item) => {
     item.addEventListener("click", () => {
-        if (item.innerText === "") { 
+        if (item.innerText === "") {
             if (turn) {
                 item.innerText = "O";
                 item.style.color = "red";
                 item.disabled = true;
+                valX.style.display = "none"
                 document.querySelector('.result').innerText = `Player X Turn`;
                 turn = false;
                 cnt++;
@@ -34,16 +48,15 @@ box.forEach((item) => {
                 cnt++;
             }
 
-
             for (let pattern of winningPatterns) {
                 let val1 = box[pattern[0]].innerText;
                 let val2 = box[pattern[1]].innerText;
                 let val3 = box[pattern[2]].innerText;
-                
+
                 if (val1 !== "" && val1 === val2 && val2 === val3) {
                     document.querySelector('.result').innerText = `Winner is player ${item.innerText}`;
                     disableAllBoxes();
-                    return; 
+                    return;
                 }
             }
 
@@ -68,6 +81,7 @@ const ResetGame = () => {
     }
     turn = true;
     valX.checked = false;
+    valX.style.display = "block"
     cnt = 0;
     document.querySelector('.result').innerText = "";
 };
