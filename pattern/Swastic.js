c8ffdb60ced0a6d46a6b2421ab99430ad805603d
NiravDhanani <nirav.devotee@gmail.com>
2024-07-30 10:40:21 +0530

    pattern continue

--- pattern/Swastic.js
+++ pattern/Swastic.js
@@ -15,7 +15,7 @@
 //                     document.write("&nbsp;".repeat((num * 2)), i)
 //                 } else {
 //                     document.write("&nbsp;&nbsp;".repeat((midpoint * 2)), i)
-                    
+
 //                 }
 //             } else if (j >= midpoint && i === 1) {
 //                 document.write("&nbsp;&nbsp;", (Math.floor(j - (num / 2 - 1))));
@@ -48,27 +48,68 @@ function pattern(num) {
 
     for (let i = 1; i <= num; i++) {
         for (let j = 1; j <= num; j++) {
+            // sleeping cener line 
             if (i === midpoint) {
-                // Horizontal middle line
-                document.write(j, "&nbsp;&nbsp;");
-            } else if (j === midpoint) {
-                // Vertical middle line
+                if (j < 10) {
+                    document.write(j, "&nbsp;&nbsp;&nbsp;")
+                } else {
+                    document.write(j, "&nbsp;&nbsp;")
+                }
+                
+            }
+
+            // standing center line
+            else if (j === midpoint) {
+                if (j < 11) {
+                    document.write(i, "&nbsp;&nbsp;&nbsp;")
+                } else {
+                    document.write(i, "&nbsp;&nbsp;")
+                }
+            }
+              // top left corner
+              else if (j === 1 && i <= midpoint) {
+                if (num.toString().length === 1) {
+                    document.write(midpoint - i + 1, "&nbsp;&nbsp;".repeat(midpoint));
+                }
+                else {
+                    document.write(midpoint - i + 1, "&nbsp;&nbsp;".repeat(midpoint));
+                }
+
+            }
+            // Top right corner
+            else if (i === 1 && j >= midpoint) {
+                if (i < 11) {
+                    document.write(j - midpoint + 1, "&nbsp;&nbsp;&nbsp;");
+                } else {
+                    document.write(j - midpoint + 1, "&nbsp;&nbsp;");
+                }
+            }
+
+            // bottom left corner
+            else if (i === num && j <= midpoint) {
+                if (num.toString().length === 2) {
+                    document.write(num - j + 1, "&nbsp;&nbsp;");
+                }
+                else {
+                    document.write(num - j + 1, "&nbsp;&nbsp;&nbsp;");
+                }
+            }
+
+
+          
+            // bottom right corner
+            else if (j === num && i >= midpoint) {
+
                 document.write(i, "&nbsp;&nbsp;");
-            } else if (i === 1 && j >= midpoint) {
-                // Top right quadrant
-                document.write(j - midpoint + 1, "&nbsp;&nbsp;");
-            } else if (i === num && j <= midpoint) {
-                // Bottom left quadrant, reversed
-                document.write(num - j + 1, "&nbsp;&nbsp;");
-            } else if (j === 1 && i <= midpoint) {
-                // Top left quadrant
-                document.write(midpoint - i + 1, "&nbsp;&nbsp;");
-            } else if (j === num && i >= midpoint) {
-                // Bottom right quadrant, reversed
-                document.write(i, "&nbsp;&nbsp;");
-            } else {
-                // Filling the rest with spaces
-                document.write("&nbsp;&nbsp;&nbsp;&nbsp;");
+            }
+            else {
+                if (num.toString().length === 1) {
+                    document.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+                }
+                else {
+                    document.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+                }
+
             }
         }
         document.write("<br>");
--- pattern/pluse.js
+++ pattern/pluse.js
@@ -1,58 +1,36 @@
 
-// let standing = 0;
-// let sleeping = 0;
-// function pattern(num) {
-//     let midpoint = Math.floor(num / 2)
-//     for (let i = 0; i < num; i++) {
-//         for (let j = 0; j < num; j++) {
-//             if (i == midpoint && j == midpoint) {
-//                 standing++;
-//                 sleeping++;
-//                 document.write(standing)
-//                 document.write("&nbsp;&nbsp;");
-//             } else if (i == midpoint && j != midpoint) {
-//                 sleeping++;
-//                 document.write(sleeping)
-//                 document.write("&nbsp;&nbsp;")
-//             }
-//             else if (i != midpoint && j == midpoint) {
-//                 standing++;
-//                 let space = ""
-//                 space = "&nbsp;&nbsp;&nbsp;&nbsp;".repeat(midpoint)
-//                 document.write(space)
-//                 document.write(standing)
-//             }
-//         }
-//         document.write("<br>")
-//     }
-// }
-
-// pattern(21)
-
-
+let standing = 0;
+let sleeping = 0;
 function pattern(num) {
-    if (num % 2 === 0) {
-        alert('Enter odd number only');
-        return;
-    }
-
-    let midpoint = Math.floor(num / 2) + 1;
-
-    for (let i = 1; i <= num; i++) {
-        for (let j = 1; j <= num; j++) {
-            if (i === midpoint) {
-                document.write(j, "&nbsp;&nbsp;");
-            } else if (j === midpoint) {
-                document.write(i, "&nbsp;&nbsp;");
-            } else {
-                document.write("&nbsp;&nbsp;&nbsp;&nbsp;");
+    let midpoint = Math.floor(num / 2)
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            if (i == midpoint && j == midpoint) {
+                standing++;
+                sleeping++;
+                document.write(standing)
+                document.write("&nbsp;&nbsp;");
+            } else if (i == midpoint && j != midpoint) {
+                sleeping++;
+                document.write(sleeping)
+                document.write("&nbsp;&nbsp;")
+            }
+            else if (i != midpoint && j == midpoint) {
+                standing++;
+                let space = ""
+                space = "&nbsp;&nbsp;&nbsp;&nbsp;".repeat(midpoint)
+                document.write(space)
+                document.write(standing)
             }
         }
-        document.write("<br>");
+        document.write("<br>")
     }
 }
 
-pattern(19);
+pattern(21)
+
+
+
 
 
 
